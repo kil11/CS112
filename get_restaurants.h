57b803de56400deb990403d8d5bfc00c92924584
kil11 <115838774+kil11@users.noreply.github.com>
2023-01-20 15:20:04 -0800

    Add files via upload

---
+++ get_restaurants.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   get_restaurants.h
+ * Author: David
+ *
+ * Created on August 27, 2021, 9:38 AM
+ */
+
+#ifndef GET_RESTAURANTS_H
+#define GET_RESTAURANTS_H
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void get_restaurants(string restaurants[], int size);
+
+#endif /* GET_RESTAURANTS_H */
+
